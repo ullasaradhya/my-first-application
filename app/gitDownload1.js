1be8e28f72ff8b2ca11628a987a9260572002fb9
ullasaradhya <ullasaradhya05@gmail.com>
2018-12-07 20:21:38 +0530

    changes

--- app/gitDownload1.js
+++ app/gitDownload1.js
@@ -1,5 +1,24 @@
 var express=require('express');
+var app=express();
+app.set('view engine', 'ejs');
+
+var files=["hello", "hi", "world", "good"];
+
+var sample="G://nodejs//code//app//public//js//scripts.js";
 
 
+app.get('/', function(req, res){
+    res.render('gitDownload', {message : sample});    
+})
 
+app.get('/reg', function(req, res){
+    res.render('fileUpload');
+})
 
+app.get('/download', function(req, res){
+    res.download('G://nodejs//code//app//public//js//scripts.js');
+})
+
+app.listen(3000, function(){
+    console.log('Server Listening at 3000');
+})
